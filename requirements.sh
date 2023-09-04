0f1cea6adff97e3d9611ecffa58b09c061de0015
jaison <jaisonjose241@gmail.com>
2023-09-04 18:36:49 +0530

    added requirements

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+sudo apt install python3-pip
+sudo pip3 install transforms3d
+sudo apt install ros-humble-gazebo-ros
+sudo apt install ros-humble-gazebo-plugins
+sudo apt install ros-humble-xacro
+sudo apt install ros-humble-tf-transformations
+sudo cp eyantra_warehouse/config/SkyX_Moon.fragment /usr/share/gazebo-11/media/skyx/SkyX_Moon.fragment
+sudo cp eyantra_warehouse/config/SkyX_Moon.png /usr/share/gazebo-11/media/skyx/SkyX_Moon.png
+source /usr/share/gazebo-11/setup.bash
